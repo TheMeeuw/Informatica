fa2033b37b4851636ed3ac1f1029c3aa4101f8d0
Lpberends <91067257+Lpberends@users.noreply.github.com>
2022-06-17 18:03:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ var blocks = setInterval(function(){
         let ihole = document.getElementById("hole"+current);
         let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
         let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
-        iblock.style.top = iblockTop - 1.5 + "px";
-        ihole.style.top = iblockTop -  1.5 + "px";
+        iblock.style.top = iblockTop - 1.2 + "px";
+        ihole.style.top = iblockTop -  1.2 + "px";
         if(iblockTop < -20){
             currentBlocks.shift();
             iblock.remove();
@@ -89,6 +89,6 @@ var blocks = setInterval(function(){
             character.style.top = characterTop + 2 + "px";
         }
     }else{
-        character.style.top = characterTop - 1.5 + "px";
+        character.style.top = characterTop - 0.5 + "px";
     }
 },1);
