213a62f67cef9c28b5687f31a9a579826f572926
Lpberends <91067257+Lpberends@users.noreply.github.com>
2023-01-12 09:53:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ var linearMovement = 0;
 var currentBlocks = [];
 
 document.addEventListener("keydown", event => {
-        if(event.key==="ArrowLeft"){
+        if(event.key==="="){
             console.log("left 1");
             inputLeft = 1;
         }
-        if(event.key==="ArrowRight"){
+        if(event.key==="L"){
             console.log("right 1");
             inputRight = 1;
         }
 });
 
 document.addEventListener("keyup", event => {
-    if(event.key==="ArrowLeft"){
+    if(event.key==="="){
         console.log("left 0");
         inputLeft = 0;
     }
-    if(event.key==="ArrowRight"){
+    if(event.key==="L"){
         console.log("right 0");
         inputRight = 0;
     }
